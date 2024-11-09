33c5268a137b74ea08c9a27f06faa66a11993ac5
danielcleves <daniel.cleves.rueda@gmail.com>
2024-11-09 16:03:06 -0500

    ✨ feat(Proveedores): Crear modelo

--- RestoStock/Models/Proveedores.cs
+++ RestoStock/Models/Proveedores.cs
@@ -2,5 +2,11 @@
 {
     public class Proveedores
     {
+        public int IdProveedor { get; set; }
+        public string NombreEmpresa { get; set; }
+        public string Contacto { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
     }
 }
