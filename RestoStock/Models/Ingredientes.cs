510ec3d884171883c947265b76f165534b240b17
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2024-11-09 15:47:58 -0500

    ✨ feat(Ingredientes): Crear Ingrediente

--- RestoStock/Models/Ingredientes.cs
+++ RestoStock/Models/Ingredientes.cs
@@ -2,6 +2,11 @@
 {
     public class Ingredientes
     {
+        public int IdIngrediente {set; get;}
+        public string Nombre {set; get;}
+        public int CantidadDisponible {set; get;}
+        public int UnidadMedida {set; get;}
+        public int PrecioUnitario {set; get;}
 
     }
 }
