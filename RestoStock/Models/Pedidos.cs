05fc4fb4eb082065c5655f2fb5390aeaa8477c96
danielcleves <daniel.cleves.rueda@gmail.com>
2024-11-09 16:34:58 -0500

    ✨ feat(Pedidos): Crear modelo

--- RestoStock/Models/Pedidos.cs
+++ RestoStock/Models/Pedidos.cs
@@ -2,5 +2,10 @@
 {
     public class Pedidos
     {
+        public int IdProveedor { get; set; }
+        public string FechaPedido { get; set; }
+        public double Total { get; set; }
+        public int FkProveedor { get; set; }
+        public Proveedores Proveedores { get; set; }
     }
 }
