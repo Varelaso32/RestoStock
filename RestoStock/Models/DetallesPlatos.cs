6f4d4aa0d298ee63add993392eec2cb931bfdb63
Samuel Varela <samyst2013@hotmail.com>
2024-11-09 16:17:53 -0500

    ✨ feat(DetallesPlatos): Actualizar DetPlatos

--- RestoStock/Models/DetallesPlatos.cs
+++ RestoStock/Models/DetallesPlatos.cs
@@ -3,8 +3,7 @@
     public class DetallesPlatos
     {
         public int IdDetalle { get; set; }
-        public int IdPlato { get; set; }
-        public int IdIngrediente { get; set; }
+
         public int Cantidad { get; set; }
 
         public int FkIngredientes { get; set; }
