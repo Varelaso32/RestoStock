0fbcab2f76039788bf952966b9e0afa3e6f97ed2
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2024-11-09 16:12:31 -0500

    ✨ feat(Platos): CrearPlatos

--- RestoStock/Models/Platos.cs
+++ RestoStock/Models/Platos.cs
@@ -2,5 +2,9 @@
 {
     public class Platos
     {
+        public int IdPlato { get; set; }
+        public int Nombre { get; set; }
+        public int PrecioVenta { get; set; }
+        public int Descripcion { get; set; }
     }
 }
