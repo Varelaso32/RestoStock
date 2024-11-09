877c47d6306cab466aec6f03da275a58c363c75f
Samuel Varela <samyst2013@hotmail.com>
2024-11-09 15:40:30 -0500

    ✨ feat(User): Crear clase User

--- RestoStock/Models/User.cs
+++ RestoStock/Models/User.cs
@@ -1,6 +1,18 @@
-﻿namespace RestoStock.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestoStock.Models
 {
     public class User
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
