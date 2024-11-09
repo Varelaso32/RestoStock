761adb3eccfbdf24e6e0be81f5c2b1a7d078d74c
danielcleves <daniel.cleves.rueda@gmail.com>
2024-11-09 16:15:35 -0500

    ✨ feat(Proveedores): Agregar ICollection

--- RestoStock/Models/Proveedores.cs
+++ RestoStock/Models/Proveedores.cs
@@ -8,5 +8,6 @@
         public string Telefono { get; set; }
         public string Email { get; set; }
         public string Direccion { get; set; }
+        public ICollection<Pedidos> Pedidos { get; set; }
     }
 }
