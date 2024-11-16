3632a984a2deed8b5a76d790f4fbf8442b5fc179
danielcleves <daniel.cleves.rueda@gmail.com>
2024-11-15 19:44:37 -0500

    ✨ feat(*): Create pages
    Create the pages for the CRUD

---
+++ RestoStock/Pages/Pedidos/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestoStock.Pages.Ingredientes
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
