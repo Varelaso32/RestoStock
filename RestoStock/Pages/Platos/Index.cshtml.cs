55a65bfd65358a62ea3f6064451a92e1856363a4
danielcleves <daniel.cleves.rueda@gmail.com>
2024-11-16 13:04:53 -0500

    ✨ feat(Plato): Index
    Add index for plato

--- RestoStock/Pages/Platos/Index.cshtml.cs
+++ RestoStock/Pages/Platos/Index.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RestoStock.BaseDeDatos.Data;
+using RestoStock.Models;
 
 namespace RestoStock.Pages.Platos
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly RestoStockContext _context;
+
+        public IndexModel(RestoStockContext context)
         {
+            _context = context;
+        }
+
+        public IList<Plato> Platos { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            // Cargar los platos desde la base de datos
+            if (_context.Platos != null)
+            {
+                Platos = await _context.Platos.ToListAsync();
+            }
         }
     }
 }
