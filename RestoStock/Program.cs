160e92d881262f12fab581badef0d8d8b508172d
Varelaso32 <samyst2013@hotmail.com>
2024-11-15 19:30:29 -0500

    ✨ feat(Program): Agregar la Inyeccion

--- RestoStock/Program.cs
+++ RestoStock/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RestoStock.BaseDeDatos.Data;
+
 namespace RestoStock
 {
     public class Program
@@ -8,6 +11,11 @@ namespace RestoStock
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<RestoStockContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Supermarket"))
+
+            );
 
             var app = builder.Build();
 
