1bbe361bf598d0bbef01d5175ecbf0753cf1f287
danielcleves <daniel.cleves.rueda@gmail.com>
2024-11-15 19:03:53 -0500

    ✨ feat(Pedidos): Change double
    Change double for int to manage all numeric properties in int

--- RestoStock/Models/Pedidos.cs
+++ RestoStock/Models/Pedidos.cs
@@ -4,7 +4,7 @@
     {
         public int IdProveedor { get; set; }
         public string FechaPedido { get; set; }
-        public double Total { get; set; }
+        public int Total { get; set; }
         public int FkProveedor { get; set; }
         public Proveedores Proveedores { get; set; }
     }
