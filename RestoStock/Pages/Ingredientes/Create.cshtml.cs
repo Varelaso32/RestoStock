213abe12ad09b7b5531cd798a0c2d7c77fdd9501
danielcleves <daniel.cleves.rueda@gmail.com>
2024-11-16 09:50:32 -0500

    ✨ feat(Ingredientes): Create
    Add create funcionality

--- RestoStock/Pages/Ingredientes/Create.cshtml.cs
+++ RestoStock/Pages/Ingredientes/Create.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestoStock.BaseDeDatos.Data;
+using RestoStock.Models;
 
 namespace RestoStock.Pages.Ingredientes
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly RestoStockContext _context;
+
+        public CreateModel(RestoStockContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Ingrediente Ingrediente { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Ingredientes == null || Ingrediente == null)
+            {
+                return Page();
+            }
+
+            _context.Ingredientes.Add(Ingrediente);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
