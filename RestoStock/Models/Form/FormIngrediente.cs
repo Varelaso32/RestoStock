e9d4c76ee12de3bf741bed6d92e930a9e0c8043f
danielcleves <daniel.cleves.rueda@gmail.com>
2024-11-16 13:31:59 -0500

    🐞 fix(Ingredientes): Correct type error

--- RestoStock/Models/Form/FormIngrediente.cs
+++ RestoStock/Models/Form/FormIngrediente.cs
@@ -8,7 +8,7 @@ namespace RestoStock.Models.Form
         public int IdIngrediente { set; get; }
         public string Nombre { set; get; }
         public int CantidadDisponible { set; get; }
-        public int UnidadMedida { set; get; }
+        public string UnidadMedida { set; get; }
         public int PrecioUnitario { set; get; }
     }
 }
