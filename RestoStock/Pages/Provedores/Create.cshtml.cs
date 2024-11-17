eda783ba228544f8b7478c19dbcc82d6d895f076
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2024-11-17 15:04:40 -0500

    ✨ feat(Proveedores): ModificoCreate

--- RestoStock/Pages/Provedores/Create.cshtml.cs
+++ RestoStock/Pages/Provedores/Create.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestoStock.Models;
 
-namespace RestoStock.Pages.Provedores
+namespace RestoStock.Pages.Proveedores
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public Proveedor Proveedor { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Lógica para guardar el Proveedor en la base de datos
+            return RedirectToPage("Index");
         }
     }
 }
